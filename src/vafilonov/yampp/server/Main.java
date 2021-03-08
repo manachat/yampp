ca7ac535d29957c8cbe26244cea668269dc58b45
manachat <vafilonov@edu.hse.ru>
2021-03-08 18:12:39 +0300

    remote init

--- src/vafilonov/yampp/server/Main.java
+++ src/vafilonov/yampp/server/Main.java
@@ -3,5 +3,12 @@ package vafilonov.yampp.server;
 public class Main {
     public static void main(String[] args) {
 
+        final int port = 10000;
+        final int nthreads = 10;
+        ServerManager manager = new ServerManager(port, nthreads);
+
+        Thread managerThread = new Thread(manager);
+
+        managerThread.start();
     }
 }
--- src/vafilonov/yampp/server/ServerManager.java
+++ src/vafilonov/yampp/server/ServerManager.java
@@ -1,20 +1,48 @@
 package vafilonov.yampp.server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ServerManager implements Runnable {
 
-    static final int PORT = 10000;
 
+
+    public ServerManager(int port, int nThreads) {
+        PORT = port;
+        N_THREADS = nThreads;
+        executor = Executors.newFixedThreadPool(N_THREADS);
+    }
+
+    private final int PORT;
+    private final int N_THREADS;
+
+    private Thread executionThread = null;
+    private volatile boolean isShutdown = false;
+    private final ExecutorService executor;
 
     @Override
     public void run() {
-        ServerSocket listener = null;
-        try (listener = new ServerSocket(PORT)) {
+        executionThread = Thread.currentThread();
+
+        try (ServerSocket listener = new ServerSocket(PORT);) {
+            while (!isShutdown && !executionThread.isInterrupted()) {
+                final Socket client = listener.accept();
+                executor.execute(new ClientHandler(client));
+            }
+        } catch (IOException ioEx) {
 
         }
 
+    }
+
+    public void shutdown() {
+        isShutdown = true;
+        executor.shutdown();
+        if (executionThread != null) {
+            executionThread.interrupt();
+        }
     }
 }
